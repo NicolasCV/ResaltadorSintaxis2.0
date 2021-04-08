32d94f243c295b3b9e2865a2334d2282ff4344c9
Nicolas CV <niccarval@hotmail.com>
2021-04-08 11:00:47 -0600

    '

--- makeHtml.py
+++ makeHtml.py
@@ -5,6 +5,9 @@ import string
 LexError=False
 ParsingError=False
 
+#Variables globales para el parser
+counter = 0
+tok=""
 tokenList=[]
 
 #Regresa que tipo de chartype es
@@ -66,31 +69,34 @@ def inputFile(filename):
     for x in EXPfs:
         tokenList.append(getCharType(x))
 
-    
+    tokenList.append("EOF")
+
     EXPfile.flush()
     EXPfile.close()
 
 #Funciones para el parser
-def ParseError():
-    global ParsingError
+def ParseError(tok):
+    global ParsingError,counter,tokenList
     ParsingError = True
+    tokenList[counter-1]="FormatERROR"
+    print("Error in token:",tok,"["+str(counter)+"]")
 
 def nextToken():
     global counter,tok,tokenList
+    tok = tokenList[counter]
     counter+=1
-    tok = tokenList[counter-1]
 
-def match(*args):
+def match(tokens):
     global tok,counter
-    print(args)
-    if tok in args:
+    print(counter-1,tok)
+
+    if tok in tokens:
         nextToken()
     else:
-        ParseError()
+        ParseError(tok)
 
 def exprRest():
     global ParsingError,tok
-    #START=>PROGRAM ID BEING CODE END
     #CODE->VAR CODE
     #CODE->ASSIGNMENT CODE 
     #CODE->E
@@ -99,63 +105,69 @@ def exprRest():
     #ASSIGNMENT-> ID = EXPR
     #EXPR -> OPERAND EXPR_REST
     #EXPR_REST-> + OPERAND EXPREST | - OPERAND EXPR_REST | * OPERAND EXPR_REST | / OPERAND EXPR_REST
-    
-    if getCharType(tok) in ["INT","FLOAT"]:
-        match(["INT,FLOAT"])
-        match(["ID"])
-        exprRest()
+    if not ParsingError:
 
-    elif getCharType(tok) in ["EQUAL"]:
-        match(["EQUAL"])
-        match(["ID","NUM","PARENTHESES"])
-        exprRest()
-    
-    elif getCharType(tok) == "NUM":
-        match(["NUM"])
-        exprRest()
+        if tok in ["INT","FLOAT"]:
+            match(["INT","FLOAT"])
+            match(["ID"])
+            match(["INT","FLOAT","ID"])
+            exprRest()
+        
+        elif tok == "NUM":
+            match(["NUM"])
+            exprRest()
 
-    elif getCharType(tok) == "ID":
-        match(["ID"])
-        match(["="])
-        exprRest()
+        elif tok == "ID":
+            match(["ID"])
+            match(["EQUAL"])
+            match(["NUM","ID","PARENTHESES"])
+            exprRest()
 
-    elif getCharType(tok) == "PARENTHESES":
-        match(["PARENTHESES"])
-        match(["+","/","-","*","ID","NUM"])
-        exprRest()
+        elif tok == "PARENTHESES":
+            print("par")
+            match(["PARENTHESES"])
+            match(["PLUS","DIVIDE","MINUS","ASTER","ID","NUM"])
+            exprRest()
 
-    elif getCharType(tok) in ["+","/","-","*"]:
-        match(["+","/","-","*"])
-        match(["ID","NUM"])
-        exprRest()
-    
-    elif getattr(tok) == "END":
-        ''
+        elif tok in ["PLUS","DIVIDE","MINUS","ASTER","ID","NUM"]:
+            match(["PLUS","DIVIDE","MINUS","ASTER","ID","NUM"])
+            match(["ID","NUM"])
+            exprRest()
 
+        elif tok == "ERROR":
+            match("ERROR")
+            exprRest()
+        
+        elif tok == "END":
+            ""
+
+        else:
+            ParsingError=True
     else:
-        ParsingError=True
-
+        print("ERROR")
     #EXPR_REST->E
     #OPERAND -> NUM | ID
     #OPERAND -> (EXPR)
 
 def stmt():
-    match(["PROGRAM"])
-    match(["ID"])
-    match(["BEGIN"])
+    #START=>PROGRAM ID BEING CODE END
+    match("PROGRAM")
+    match("ID")
+    match("BEGIN")
     exprRest()
+    match("END")
 
 #Parser que lanza error si esta mal el formato
 def parse():
     global tok
     nextToken()
     stmt()
-    match('END')
+    #match("END")
 
 
 #Funcion para hacer el output a HTML
 def output():
-    global ParsingError,LexError, EXPfs
+    global ParsingError,LexError, EXPfs,tokenList
 
     #Se abre el html en el que se pondra
     with open(R"output.html","w") as myFile:
@@ -168,41 +180,41 @@ def output():
         for i in range(len(EXPfs)):
             
             #Se comienza el primer header con programa,id,begin
-            if getCharType(EXPfs[i]) == "PROGRAM":
+            if tokenList[i] == "PROGRAM":
                 myFile.write('<h3>')
 
             #Siempre que hay int,float,begin o end se pone un endline antes (excepto en la primera)
-            if getCharType(EXPfs[i]) in ["BEGIN","INT","FLOAT","END"]:
+            if tokenList[i] in ["BEGIN","INT","FLOAT","END"]:
                 if getCharType(EXPfs[i-1]) != "BEGIN":
                     myFile.write('<br>')
 
             #Se abre el header para el end 
-            if getCharType(EXPfs[i]) == "END":
+            if tokenList[i] == "END":
                 myFile.write('<h3>') 
 
             #Si hay dos IDs seguidas,hay un endline entre ellos o un numero antes, siempre y cuando este empezado el programa
-            if getCharType(EXPfs[i]) == "ID" and getCharType(EXPfs[i-1]) in ["ID","NUM"] and programBegun==True:
+            if tokenList[i] == "ID" and getCharType(EXPfs[i-1]) in ["ID","NUM"] and programBegun==True:
                 myFile.write("<br><span class='INDENT'></span>")          
 
             #Imprime la linea con el texto y su respectiva clase
-            line="<span class='{0}'>{1} ".format(getCharType(EXPfs[i]),EXPfs[i])
+            line="<span class='{0}'>{1} ".format(tokenList[i],EXPfs[i])
             myFile.write(line)
 
             #Se cierra el primer header con programa,id,begin y se comienza el codigo
-            if getCharType(EXPfs[i]) == "BEGIN":
+            if tokenList[i] == "BEGIN":
                 myFile.write('</span></h3>\n')
                 programBegun=True
                 continue
             
             #Se cierra el header para el end
-            if getCharType(EXPfs[i]) == "END":
+            if tokenList[i] == "END":
                 myFile.write('</h3>')
                 continue
             
             #Se cierra el span
             myFile.write('</span>\n')
 
-            if getCharType(EXPfs[i])=="ERROR":
+            if tokenList[i]=="ERROR":
                 LexError=True
 
         #Da los errores abajo de todo el programa para que el usuario sepa que hubo error
@@ -219,10 +231,7 @@ def output():
  
 
 inputFile("inputFileOne.txt")
-counter = 0
-tok=""
 parse()
-
 output()
 
 
